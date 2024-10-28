426c8d1d91d650d0e5b7c99a53c7c9f399fc64f1
Cynthia Phan <cynthiaphan.code@gmail.com>
2024-10-27 20:39:54 -0400

    add feature: add task with 'Enter' key

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -57,10 +57,12 @@ const ToDo = () => {
       </div>
 
       {/* input */}
-      <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-        <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-400' type="text" placeholder='Add your task' />
-        <button onClick={addTodo} className='border-none rounded-full bg-[#b2ae77] w-32 h-14 text-[#fefcf6] text-lg font-medium cursor-pointer'>ADD</button>
-      </div>
+      <form onSubmit={addTodo} className='flex items-center my-7 bg-gray-200 rounded-full'>
+        {/* <form  className=''> */}
+          <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-400' type="text" placeholder='Add your task' />
+          <button className='border-none rounded-full bg-[#b2ae77] w-32 h-14 text-[#fefcf6] text-lg font-medium cursor-pointer' type='submit'>ADD</button>
+        {/* </form> */}
+      </form>
 
       {/* to do list */}
       <div>
